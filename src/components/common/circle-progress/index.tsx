c7c5a6c35844f17ef2e5b523a3b565c5f6d3ad7e
vecchio <weichao@zbj.com>
2019-03-13 16:24:41 +0800

    feat:中国色简单页面搭建

---
+++ src/components/common/circle-progress/index.tsx
@@ -0,0 +1,67 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+
+import './index.scss'
+
+const defaultColor = '#6FEC6F'
+
+type PageStateProps = {
+  max?: number,
+  current: number,
+  showNumber?: boolean,
+  format?: Function,
+  color?: string
+}
+
+interface CircleProgress {
+  props: PageStateProps;
+}
+
+class CircleProgress extends Component {
+
+  render() {
+    const {
+      current = 0,
+      showNumber = false,
+      format = null,
+      color
+    } = this.props
+    let leftStyle;
+    let rightStyle;
+    let maskStyle;
+    if (current <= 50) {
+      leftStyle = { transform: `rotate(${3.6 * current}deg)` }
+      rightStyle = { opacity: 0 }
+      maskStyle = { opacity: 1 }
+    } else {
+      leftStyle = { transform: `rotate(180deg)` }
+      rightStyle = {
+        opacity: 1,
+        transform: `rotate(${3.6 * current}deg)`
+      }
+      maskStyle = { opacity: 0 }
+    }
+    const showColor = color || defaultColor
+    leftStyle.borderColor = showColor
+    rightStyle.borderColor = showColor
+    return (
+      <View className="circle-progress">
+        <View className="circle">
+          <View className="track" />
+          <View className="left transition-rotate" style={leftStyle} />
+          <View className="right transition-rotate" style={rightStyle} />
+          <View className="mask" style={maskStyle} />
+          {
+            showNumber && (
+              <Text className="hour">
+                { format ? format(current) : current }
+              </Text>
+            )
+          }
+        </View>
+      </View>
+    )
+  }
+}
+
+export default CircleProgress
--- src/pages/color/index.tsx
+++ src/pages/color/index.tsx
@@ -1,43 +1,137 @@
 import { ComponentType } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import { AtCalendar } from 'taro-ui'
 
+import CircleProgress from '../../components/common/circle-progress'
+
+import colorData from './data.json'
 import './index.scss'
 
-type PageStateProps = {
+const CMYK_CONFIG = [{
+  type: 'C',
+  color: '#0093D3'
+}, {
+  type: 'M',
+  color: '#CC006B'
+}, {
+  type: 'Y',
+  color: '#FFF10C'
+}, {
+  type: 'K',
+  color: '#333333'
+}]
+const RGB_CONFIG = [{
+  type: 'R',
+  color: 'red'
+}, {
+  type: 'G',
+  color: 'green'
+}, {
+  type: 'B',
+  color: 'blue'
+}]
 
+type PageStateProps = {
 }
 
-interface Appointment {
+type ColorItemType = {
+  CMYK: number[],
+  RGB: number[],
+  hex: string,
+  name: string,
+  pinyin: string
+}
+
+type StateType = {
+  color: ColorItemType
+}
+
+interface Color {
   props: PageStateProps;
 }
 
-class Appointment extends Component {
+class Color extends Component {
+
+  state: StateType = {
+    color: colorData[0]
+  }
 
   config: Config = {
-    navigationBarTitleText: '日期安排'
+    navigationBarTitleText: '中国色'
   }
 
-  clickDate = (date) => {
-    console.log(date)
+  setTitle = (title) => {
+    const showTitle = title || '中国色'
+    Taro.setNavigationBarTitle({ title: showTitle })
   }
 
-  longClickDate = (date) => {
-    console.log(date)
+  modifyColor = (color: ColorItemType) => {
+    this.setState({ color })
+    this.setTitle(color.name)
+  }
+
+  componentDidMount () {
+    this.setTitle(this.state.color.name)
   }
 
   render () {
+    const {
+      CMYK,
+      RGB
+    } = this.state.color
+    const cmykItemList = CMYK.map((item, index) => {
+      return (
+        <View key={item} className="color-cmyk-item">
+          <CircleProgress
+            current={item}
+            showNumber={true}
+            color={CMYK_CONFIG[index].color}
+          />
+        </View>
+      )
+    })
+    const rgbItemList = RGB.map((item, index) => {
+      return (
+        <View
+          key={item}
+          className="color-rgb-item"
+          style={{color: RGB_CONFIG[index].color}}
+        >
+          {RGB_CONFIG[index].type}:{item}
+        </View>
+      )
+    })
+    const colorItemList = colorData.map((color: ColorItemType) => {
+      const {
+        hex,
+        name,
+        pinyin,
+        CMYK,
+        RGB
+      } = color
+      return (
+        <View className="color-item" key={hex} onClick={() => this.modifyColor(color)}>
+          <View className="color-show" style={`background-color: ${hex}`} />
+          <View className="color-name">{name}</View>
+        </View>
+      )
+    })
     return (
-      <View className="appointment">
-        <AtCalendar
-          isVertical
-          onDayClick={this.clickDate}
-          onDayLongClick={this.longClickDate}
-        />
+      <View className="color">
+        <View className="color-select-wrap">
+          <View className="color-cmyk-wrap">
+            {cmykItemList}
+          </View>
+          <View className="color-rgb-wrap">
+            {rgbItemList}
+          </View>
+        </View>
+        <View className="color-list" style={{backgroundColor: this.state.color.hex}}>
+          {colorItemList}
+        </View>
       </View>
     )
   }
 }
 
-export default Appointment as ComponentType
+export default Color as ComponentType
