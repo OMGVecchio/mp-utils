f97a38b836f9e689eea4d358da5e8acef131747c
vecchio <weichao@zbj.com>
2019-08-13 15:10:35 +0800

    删除域名后的端口

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,10 @@
-export const SERVER_HOST = '172.31.10.104'
-export const SERVER_PORT = '3000'
-export const SERVER_HTTP = `http://${SERVER_HOST}:${SERVER_PORT}`
-export const SERVER_SOCKET = `ws://${SERVER_HOST}:${SERVER_PORT}`
+// export const SERVER_HOST = '172.31.10.104'
+// export const SERVER_PORT = '3000'
+// export const SERVER_HTTP = `http://${SERVER_HOST}:${SERVER_PORT}`
+// export const SERVER_SOCKET = `ws://${SERVER_HOST}:${SERVER_PORT}`
+export const SERVER_HOST = 'vecchio.top'
+export const SERVER_PORT = '443'
+export const SERVER_HTTP = `https://${SERVER_HOST}`
+export const SERVER_SOCKET = `wss://${SERVER_HOST}`
 
 export default SERVER_HOST
